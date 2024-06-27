3ad255eb7ab8d99e19a2b8653101ef5b1b3f9110
rajeshmondal54 <rm4223536@gmail.com>
2024-06-27 13:39:14 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,11 +1,10 @@
-//your JS code here. If required.
 document.addEventListener("DOMContentLoaded", function() {
     var openModalButton = document.getElementById("openModal");
     var modal = document.getElementById("modal");
     var closeModalButton = document.querySelector(".close-modal");
 
     openModalButton.addEventListener("click", function() {
-        modal.style.display = "flex";
+        modal.style.display = "block";
     });
 
     closeModalButton.addEventListener("click", function() {
