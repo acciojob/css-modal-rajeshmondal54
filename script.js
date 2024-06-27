7c9c14248b643c5ea0a120664dca1e061d185e9b
rajeshmondal54 <rm4223536@gmail.com>
2024-06-27 13:34:47 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,20 @@
 //your JS code here. If required.
+document.addEventListener("DOMContentLoaded", function() {
+    var openModalButton = document.getElementById("openModal");
+    var modal = document.getElementById("modal");
+    var closeModalButton = document.querySelector(".close-modal");
+
+    openModalButton.addEventListener("click", function() {
+        modal.style.display = "flex";
+    });
+
+    closeModalButton.addEventListener("click", function() {
+        modal.style.display = "none";
+    });
+
+    window.addEventListener("click", function(event) {
+        if (event.target == modal) {
+            modal.style.display = "none";
+        }
+    });
+});
